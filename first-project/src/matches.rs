84c5195c744fc5358f69ca6159cfaedcd15a326f
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2022-06-11 20:54:07 +0100

    Update matches.rs

--- first-project/src/matches.rs
+++ first-project/src/matches.rs
@@ -11,4 +11,22 @@ pub fn run(){
         21..=40 => println!("Class C"),
         _ => println!("Class not registered")
     }
+
+    enum Coin{
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u32{
+        match coin{
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    println!("Value of quarter is: {}", value_in_cents(Coin::Quarter));
 }
